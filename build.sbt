1408a9de1cc671ef1a44f5ce7b6209e8fd2f56bc
murosan <masashi_16n1200013@nnn.ed.jp>
2017-12-16 01:53:10 +0900

    挑戦上級

--- build.sbt
+++ build.sbt
@@ -7,6 +7,8 @@ scalaVersion := "2.12.3"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.3",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.3" % Test,
+  "com.typesafe.akka" %% "akka-stream" % "2.5.8",
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.8" % Test,
   "com.squareup.okhttp3" % "okhttp" % "3.8.1",
   "com.typesafe" % "config" % "1.3.1"
 )
--- src/main/scala/jp/ed/nnn/imagedownloader/Supervisor.scala
+++ src/main/scala/jp/ed/nnn/imagedownloader/Supervisor.scala
@@ -2,27 +2,36 @@ package jp.ed.nnn.imagedownloader
 
 import java.util.concurrent.TimeUnit
 
+import akka.NotUsed
 import akka.actor.{Actor, ActorRef, Props}
-import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+import akka.pattern.ask
+import akka.routing.RoundRobinPool
+import akka.stream.{ActorMaterializer, OverflowStrategy, ThrottleMode}
+import akka.stream.scaladsl.{Flow, Source}
+import akka.util.Timeout
 import okhttp3._
 
-import scala.io.Source
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+import scala.io.{Codec, Source => IOSource}
 
 class Supervisor(config: Config) extends Actor {
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContextExecutor = context.dispatcher
+  implicit val askTimeout: Timeout = Timeout(100.days)
   var originalSender: ActorRef = Actor.noSender
 
   var successCount = 0
   var failureCount = 0
-  var fileLoadedUrlCount = 0
 
-  val wordsFileSource: Source = Source.fromFile(config.wordsFilePath)
-  val wnidWordMap: Map[String, String] =
-    wordsFileSource.getLines()
-      .map(s => {
-        val strs = s.split("\t")
-        (strs.head, strs.tail.mkString("\t"))
-      }).toMap
-
+  val wordsFileSource: IOSource = IOSource.fromFile(config.wordsFilePath)
+  val wnidWordMap: Map[String, String] = wordsFileSource
+    .getLines()
+    .map { s =>
+      val Array(wnid, word) = s.split("\t")
+      wnid -> word
+    }
+    .toMap
 
   val client: OkHttpClient = new OkHttpClient.Builder()
     .connectTimeout(1, TimeUnit.SECONDS)
@@ -30,40 +39,47 @@ class Supervisor(config: Config) extends Actor {
     .readTimeout(1, TimeUnit.SECONDS)
     .build()
 
-  val router: Router = {
-    val downloaders = Vector.fill(config.numOfDownloader) {
-      ActorRefRoutee(context.actorOf(
-        Props(new ImageFileDownloader(
-          config,
-          client,
-          wnidWordMap
-        ))))
-    }
-    Router(RoundRobinRoutingLogic(), downloaders)
-  }
+  val ref: ActorRef = context.actorOf(
+    Props(new ImageFileDownloader(config, client, wnidWordMap))
+      .withRouter(new RoundRobinPool(config.numOfDownloader)))
+
+  val urlsFileSource: IOSource =
+    IOSource.fromFile(config.urlsFilePath)(Codec.UTF8)
+  val source: Source[String, NotUsed] =
+    Source.fromIterator(() => urlsFileSource.getLines())
 
   override def receive: Receive = {
     case Start =>
       originalSender = sender()
-      val urlsFileLoader = context.actorOf(Props(new UrlsFileLoader(config)))
-      urlsFileLoader ! LoadUrlsFile
-
-    case imageNetUrl: ImageNetUrl =>
-      fileLoadedUrlCount += 1
-      router.route(imageNetUrl, self)
-
-    case DownloadSuccess(_, _) =>
-      successCount += 1
-      printConsoleAndCheckFinish()
-
-    case DownloadFailure(_, _) =>
-      failureCount += 1
-      printConsoleAndCheckFinish()
+      source
+        .via(convertToImageNetUrl)
+        .throttle(100, 1.second, 10, ThrottleMode.shaping)
+        .buffer(config.numOfDownloader, OverflowStrategy.backpressure)
+        .via(askDownload)
+        .via(countUp)
+        .runForeach(_ => printConsole())
+        .onComplete(_ => originalSender ! Finished)
   }
 
-  private[this] def printConsoleAndCheckFinish(): Unit = {
+  private[this] def convertToImageNetUrl: Flow[String, ImageNetUrl, NotUsed] =
+    Flow[String].map { line =>
+      val Array(id, url) = line.split("\t")
+      val wnid = id.split("_").head
+      ImageNetUrl(id, url, wnid)
+    }
+
+  private[this] def askDownload: Flow[ImageNetUrl, Message, NotUsed] =
+    Flow[ImageNetUrl]
+      .mapAsyncUnordered(config.numOfDownloader)(i => (ref ? i).mapTo[Message])
+
+  private[this] def countUp: Flow[Message, Unit, NotUsed] =
+    Flow[Message].map {
+      case DownloadSuccess(_, _) => successCount += 1
+      case DownloadFailure(_, _) => failureCount += 1
+    }
+
+  private[this] def printConsole(): Unit = {
     val total = successCount + failureCount
     println(s"total: $total, successCount: $successCount, failureCount: $failureCount")
-    if (total == fileLoadedUrlCount) originalSender ! Finished
   }
 }
