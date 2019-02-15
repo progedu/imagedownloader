7b74d08e3ad75ef71b5c62998f9f46f58c4d1105
yuki <yuukikokubo.pg@gmail.com>
2019-02-15 15:11:40 +0900

    update main

--- src/main/scala/jp/ed/nnn/imagedownloader/Main.scala
+++ src/main/scala/jp/ed/nnn/imagedownloader/Main.scala
@@ -7,9 +7,9 @@ import akka.stream.scaladsl._
 
 object Main extends App {
   // TODO please fix to your configuration
-  val wordsFilePath = "/Users/kokuboyuuki/workspace/imagedawnloaderUtils/util/words.txt"
-  val urlsFilePath = "/Users/kokuboyuuki/workspace/imagedawnloaderUtils/util/fall11_urls.txt"
-  val outputDirPath = "/Users/kokuboyuuki/workspace/imagedawnloaderUtils/images"
+  val wordsFilePath = "/.../.../workspace/imagedawnloaderUtils/util/words.txt"
+  val urlsFilePath = "/.../.../workspace/imagedawnloaderUtils/util/fall11_urls.txt"
+  val outputDirPath = "/.../.../workspace/imagedawnloaderUtils/images"
   val numOfDownloader = 20
   implicit val config: Config = Config(
     wordsFilePath,
